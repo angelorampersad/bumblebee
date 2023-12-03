a1b440e0948c81c8031b698560b35fb603fa2aab
angelo-rampersad <angelo.rampersad@justeattakeaway.com>
2023-12-03 15:17:42 +0100

    Add authentication route

--- client/src/components/main-nav.tsx
+++ client/src/components/main-nav.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link"
+import { usePathname } from 'next/navigation'
 
 import { cn } from "@/lib/utils"
 
@@ -6,6 +9,10 @@ export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname();
+  const activeLink = "text-sm font-medium transition-colors hover:text-primary";
+  const unactiveLink = "text-sm font-medium text-muted-foreground transition-colors hover:text-primary";
+
   return (
     <nav
       className={cn("flex items-center space-x-4 lg:space-x-6", className)}
@@ -13,25 +20,25 @@ export function MainNav({
     >
       <Link
         href="/"
-        className="text-sm font-medium transition-colors hover:text-primary"
+        className={pathname === '/' ? activeLink : unactiveLink}
       >
         Overview
       </Link>
       <Link
-        href="/"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+        href="/insights"
+        className={pathname === '/insights' ? activeLink : unactiveLink}
       >
         Insights
       </Link>
       <Link
-        href="/"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+        href="/integrations"
+        className={pathname === '/integrations' ? activeLink : unactiveLink}
       >
         Integrations
       </Link>
       <Link
-        href="/"
-        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+        href="/settings"
+        className={pathname === '/settings' ? activeLink : unactiveLink}
       >
         Settings
       </Link>
