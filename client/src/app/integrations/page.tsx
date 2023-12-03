812f4ea4516f18dbe877ce08cb2e22ad46aaa5cd
angelo-rampersad <angelo.rampersad@justeattakeaway.com>
2023-12-03 17:39:52 +0100

    Add prisma integration

--- client/src/app/integrations/page.tsx
+++ client/src/app/integrations/page.tsx
@@ -3,6 +3,7 @@ import path from "path"
 import { Metadata } from "next"
 import Image from "next/image"
 import { z } from "zod"
+import prisma from '@/lib/db'
 
 import { columns } from "@/components/columns"
 import { DataTable } from "@/components/data-table"
@@ -44,7 +45,7 @@ export default async function TaskPage() {
           </div>
         </div>
       <div className="md:hidden">
-        <Image
+        {/* <Image
           src="/examples/tasks-light.png"
           width={1280}
           height={998}
@@ -57,7 +58,7 @@ export default async function TaskPage() {
           height={998}
           alt="Playground"
           className="hidden dark:block"
-        />
+        /> */}
       </div>
       <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
