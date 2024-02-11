0795ce83c981605b394ad8ff77c613061e6b71ef
angelorampersad <angelorampersad94@gmail.com>
2024-02-11 20:29:13 +0100

    Minor: add s3 acl

--- .infrastructure/resources.tf
+++ .infrastructure/resources.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "state" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "state" {
   bucket = aws_s3_bucket.state.id
   acl    = "private"
 }
