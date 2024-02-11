77c17d299f86940def5754dd9db1b46ca3ae5847
angelorampersad <angelorampersad94@gmail.com>
2024-02-11 20:42:24 +0100

    Minor: add tf state

--- .infrastructure/resources.tf
+++ .infrastructure/resources.tf
@@ -11,3 +11,10 @@ resource "aws_s3_bucket_acl" "state" {
   bucket = aws_s3_bucket.state.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_ownership_controls" "tfstate" {
+  bucket = aws_s3_bucket.state.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
